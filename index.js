d4a52470fb07cd1ab874d5dd71bb6699f172ac3a
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-06-02 02:08:32 +0300

    LiveReload added.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var bundleScripts = require('./lib/bundle-scripts');
 var bundleStyles = require('./lib/bundle-styles');
 var async = require('async');
 var path = require('path');
+var livereload = require('./lib/livereload');
 
 function fosify(opts) {
   var currentPath = path.resolve(process.cwd());
@@ -15,6 +16,9 @@ function fosify(opts) {
   opts.dest = path.join(currentPath, opts.dest || './build');
 
   async.applyEachSeries([bundleScripts, bundleStyles], opts, function(err) {
+    if (opts.livereload) {
+      livereload(opts);
+    }
   });
 }
 
