a9bd7b9f258c09b5883f1178c1d2c1211c94933b
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-06-14 15:49:42 +0300

    Browserify replace changed with insertGlobalVars.

--- example/scripts/hosts.bundle.js
+++ example/scripts/hosts.bundle.js
@@ -1,4 +1,4 @@
-console.log('__host');
-console.log('__secureHost');
-console.log('__host');
-console.log('__secureHost');
+console.log(__host);
+console.log(__secureHost);
+console.log(__host);
+console.log(__secureHost);
